fc1fe0513958b01949bc773ff075db02e575146b
PastelDeFeira <kirrby.gaming@gmail.com>
2023-08-30 12:26:01 -0300

    agora da pra checar

--- principal.py
+++ principal.py
@@ -1,36 +1,37 @@
 import json
 import random
-import re
 
-def game_loop():
-    # load all 4 letter words
-    with open('4_letter_words.json', 'r') as file:
-        words_data = json.load(file)
+# load all 4 letter words
+with open('4_letter_words.json', 'r') as file:
+    words_data = json.load(file)
 
-    # load words list as list
-    words_list = [entry['word'].lower() for entry in words_data]
+# load words list as list
+words_list = [entry['word'].lower() for entry in words_data]
 
-    # choose a random word from the list
-    random_word = random.choice(words_list)
-    print(random_word)
+# choose a random word from the list
+random_word = random.choice(words_list)
+print(random_word)
 
-    # split random word into individual characters
-    word_chars = list(str(random_word))
-    print(word_chars)
+# split random word into individual characters
+word_chars = list(str(random_word))
+print(word_chars)
 
-    user_guess = input("Guess a 4 letter word: ")
-    user_chars = list(user_guess.lower())
+def guess():
+    while 2:
+        user_guess = input("Guess a 4 letter word: ")
+        user_chars = list(user_guess.lower())
 
-    # limits user's guess to 4 chars only
-    if len(user_guess) != 4:
-        print("Use only letters for your guess!")
+        # limits user's guess to 4 chars only
+        if len(user_guess) != 4:
+            print("Use only letters for your guess!")
+            break
 
-    # makes sure user's guess is a valid word
-    if user_guess not in words_list:
-        print("Enter a valid word!")
-        print("a")
+        # makes sure user's guess is a valid word
+        if user_guess not in words_list:
+            print("Enter a valid word!")
+            break
 
-    print(user_chars)
+    #print(user_chars)
 
-while True:
-    game_loop()
+while 1:
+    guess()
